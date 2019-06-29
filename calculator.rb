3b84b8429a0f348b0b54d3add9bcf5a55e8c0757
David Little <littledavid175@gmail.com>
2019-06-29 17:15:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,8 @@
 # Add your variables here
 first_number=5
 second_number=3
-def sum(first_number,second_number)
-  "#{first_number} + #{second_number}"
-end
+sum= first_number + second_number
+ 
 
 def difference(first_number,second_number)
   "#{first_number} - #{second_number}"
@@ -16,7 +15,7 @@ end
 def quotient(first_number,second_number)
   "#{first_number} / #{second_number}"
 end
-sum(first_number,second_number)
+
 difference(first_number,second_number)
 product(first_number,second_number)
  quotient(first_number,second_number)
