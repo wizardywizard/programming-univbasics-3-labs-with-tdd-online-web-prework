44c0da1fc95a6b1aaf6946a342be784e4ca4c640
David Little <littledavid175@gmail.com>
2019-06-29 17:16:30 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,18 +4,8 @@ second_number=3
 sum= first_number + second_number
  
 
-def difference(first_number,second_number)
-  "#{first_number} - #{second_number}"
-end
+difference= first_number - second_number
   
-def product(first_number,second_number)
-  "#{first_number} * #{second_number}"
-end
+product= first_number * second_number
 
-def quotient(first_number,second_number)
-  "#{first_number} / #{second_number}"
-end
-
-difference(first_number,second_number)
-product(first_number,second_number)
- quotient(first_number,second_number)
+quotient= first_number / second_number
